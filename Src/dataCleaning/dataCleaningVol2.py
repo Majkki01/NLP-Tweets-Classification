1bb68d3384e25a88063041f24493a5015c20d858
Michał Rejmak <98990193+Majkki01@users.noreply.github.com>
2022-11-23 22:00:21 +0100

    Private/mrejmak/data reduction and format (#3)
    * cleaning data vol2 task - functions implementation
    
    * requested return added

---
+++ Src/dataCleaning/dataCleaningVol2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def lowercase(data):
+    data['Text'] = data['Text'].str.lower()
+    return data
+
+
+def removeURL(data):
+    data['Text'] = data['Text'].str.replace(r' *http[^ ]*', '')
+    data['Text'] = data['Text'].str.replace(r' *www[^ ]*', '')
+    return data
+
+
+def removeTags(data):
+    data['Text'] = data['Text'].str.replace('#', '')
+    data['Text'] = data['Text'].str.replace(r' *@[^ ]*', '')
+    return data
