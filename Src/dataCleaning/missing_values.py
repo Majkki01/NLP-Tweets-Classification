c9f8a940b1c62c3e98c46dc486ad9da907d6a159
Majkki01 <michal70005@gmail.com>
2022-11-23 20:31:09 +0100

    cleaned and reduced dataset in parquet format

---
+++ Src/dataCleaning/missing_values.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Reading dataCleaning from csv
+dataset = pd.read_csv('../../dataset/dirtydataset.csv')
+
+# Bar plot of missing values
+nulls = dataset.isnull().sum().sort_values(ascending=False)
+nulls.plot(
+    kind='bar', figsize=(13, 10))
+plt.title('Missing values in the dataset')
+plt.xlabel('Column title')
+plt.ylabel('Number of missing values')
+plt.show()
+
+# Filtering dataset
+dataset['Language'].value_counts().head(20)
+df_eng = dataset[dataset['Language'] == 'en'].reset_index(drop=True)
+df_eng.sample(frac=0.8).to_parquet('dataset.parquet')
+
+
