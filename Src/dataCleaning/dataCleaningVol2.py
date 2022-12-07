d91389c6179cdf5eed8b826d164e1cd4d4e65e54
Tomek Kuczyński <81151030+dev-tomek@users.noreply.github.com>
2022-12-07 23:33:53 +0100

    cleaning data final version

--- Src/dataCleaning/dataCleaningVol2.py
+++ Src/dataCleaning/dataCleaningVol2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 
 def lowercase(data):
@@ -7,12 +8,23 @@ def lowercase(data):
 
 
 def removeURL(data):
-    data['Text'] = data['Text'].str.replace(r' *http[^ ]*', '')
-    data['Text'] = data['Text'].str.replace(r' *www[^ ]*', '')
+    data['Text'] = data['Text'].str.replace(r' *http[^ ]*', '', regex=True)
+    data['Text'] = data['Text'].str.replace(r' *www[^ ]*', '', regex=True)
     return data
 
 
 def removeTags(data):
     data['Text'] = data['Text'].str.replace('#', '')
-    data['Text'] = data['Text'].str.replace(r' *@[^ ]*', '')
+    data['Text'] = data['Text'].str.replace(r' *@[^ ]*', '', regex=True)
     return data
+
+
+def removeNewLine(data):
+    data['Text'] = data['Text'].str.replace('\n', ' ')
+    return data
+
+
+def clean_text(data): 
+    data['Text'] = data['Text'].str.replace(r'[^A-Za-z0-9\s]', '', flags=re.UNICODE, regex=True)
+    return data
+
