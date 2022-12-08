d730ce87097909c3bf7f9a3ad5029e3ba1bfed6b
Michał Rejmak <98990193+Majkki01@users.noreply.github.com>
2022-12-08 01:46:38 +0100

    hashtag analysis + corrected clean pipeline loaded (#8)

--- Src/dataCleaning/cleanDatasetPipeline.py
+++ Src/dataCleaning/cleanDatasetPipeline.py
@@ -2,17 +2,19 @@ import os
 from dataCleaningVol2 import *
 
 
-#to be changed
+# to be changed
 data_path = f'{os.path.dirname(os.path.dirname(os.getcwd()))}/dataset/' 
 data = pd.read_parquet(f'{data_path}/dataset.parquet')
 
+
 def run_cleaning_pipeline(data):
-    #to be improved
+    # to be improved
     data = lowercase(data)
     data = removeURL(data)
     data = removeTags(data)
     data = removeNewLine(data)
     data = clean_text(data)
     return data
+
 
 run_cleaning_pipeline(data).to_parquet(f'{data_path}/dataset_clean.parquet')
