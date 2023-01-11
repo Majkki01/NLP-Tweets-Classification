2a520fbd219229145b5488009d29166769b5a2fa
Jacobi <97886316+JS-Kuba@users.noreply.github.com>
2023-01-11 22:01:34 +0100

    Private/kuba/stratified sampling (#19)
    * feat: add stratified sampling
    
    * fix: enlarge sampled dataset for training
    
    * fix: add sampling fraction argv
    
    * fix: merge sampling

--- Src/model/model_training.py
+++ Src/model/model_training.py
@@ -79,10 +79,10 @@ def train(model, train_data, val_data, learning_rate, epochs):
 if __name__ == "__main__":
     epochs = int(sys.argv[1])  # no. of epochs
     # head = int(sys.argv[2])  # number to be extracted from dataset
-    # sample_frac = float(sys.argv[3])  # fraction of head to be passed to the model
+    sample_frac = float(sys.argv[2])  # fraction of head to be passed to the model
 
     data = pd.read_parquet(DATA_SAMPLE)
-    data = data.sample(frac=1)
+    data = data.sample(frac=sample_frac)
     # splitting dataset into train set (80%), validation set (10%) and test set (10%)
     data_train, data_val, data_test = np.split(data.sample(frac=1, random_state=42),
                                                [int(.8 * len(data)), int(.9 * len(data))])
