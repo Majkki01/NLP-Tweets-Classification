3cf50f39f4a24602b0e32ed666f8c158aa1d5c2f
Michał Rejmak <98990193+Majkki01@users.noreply.github.com>
2023-01-11 19:23:03 +0100

    data_paths (#15)

---
+++ Src/model/definitions.py
@@ -0,0 +1,11 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_DIR = os.path.join(ROOT_DIR, '../../dataset')
+
+DATA_PATH = os.path.join(DATA_DIR, 'dataset_clean.parquet')
+
+SAVE_MODEL_PATH = os.path.join(DATA_DIR, 'trained_model.pt')
+
+print(DATA_PATH)
